ee8c175de337ce42dc35c444d9ffb5b842e72c64
Alejandro González Parra <agonzalezp@iesrafaelalberti.es>
2021-12-01 21:37:22 +0100

    Documentado y en funciones

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,32 +1,30 @@
 import java.io.File
 
 
-fun main() {
-    val lista= mutableListOf<Int>()
-    val a:File = File("C:\\Users\\usuarioT\\IdeaProjects\\advent_of_code_2021\\input\\input_day1.txt")
-    a.forEachLine { lista.add(it.toInt()) }
-
+fun contarIncrementos(lista: List<Int>):Int{
     var counter = 0
-
     for (i in 1 until lista.size){
         if (lista[i]>lista[i-1]) counter++
     }
+    return counter
+}
 
-    println(counter)
+fun main() {
+    val listaEscaner= mutableListOf<Int>()
+    val a:File = File("C:\\Users\\usuarioT\\IdeaProjects\\advent_of_code_2021\\input\\input_day1.txt")
+    a.forEachLine { listaEscaner.add(it.toInt()) }
+    
+    println("Incrementos de un escaneo a otro.")
+    println(contarIncrementos(listaEscaner))
 
-    val newList = mutableListOf<Int>()
+    val listaEscanerEnVentana = mutableListOf<Int>()
 
-    for(i in 0 until lista.size-2){
-        val it = lista[i] + lista[i+1] + lista[i+2]
-        newList.add(it)
+    //Sumo los 3 elementos correspondientes y los añado a la nueva lista.
+    for(i in 0 until listaEscaner.size-2){
+        val it = listaEscaner[i] + listaEscaner[i+1] + listaEscaner[i+2]
+        listaEscanerEnVentana.add(it)
 
     }
-
-    counter = 0
-
-    for(i in 1 until newList.size){
-        if (newList[i]>newList[i-1]) counter++
-    }
-
-    println(counter)
+    println("Incrementos sumados en ventana:")
+    println(contarIncrementos(listaEscanerEnVentana))
 }
